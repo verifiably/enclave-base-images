ea26efc2e74fa936929c2e7be7727630479a2478
spassos <spassos98@gmail.com>
2022-03-25 13:41:54 -0500

    Added python base image

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# enclave-base-images
+# Enclave Base Images
+
 This repository contains the Verifiably base docker images to run vFunctions.
---
+++ python-base-image/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rngd -v
+/gvisor-tap-vsock-main/bin/vm -debug &> /dev/null &
+sleep 10
+mkdir -p /run/resolvconf
+echo "nameserver 192.168.127.1" > /run/resolvconf/resolv.conf
+service ssh start
+sleep infinity
